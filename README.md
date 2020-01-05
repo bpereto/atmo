50c1d1dde94aa9bc4d8abd2da7e20584edc07297
Benjamin Pereto <benjamin@sandchaschte.ch>
2020-01-05 14:31:48 +0100

    FEATURE: add docs

--- README.md
+++ README.md
@@ -1,3 +1,63 @@
-# atmo
+# Atmo - Weather Station
 
-Atmo Weather Station
+"Atmo" is a homemade indoor/outdoor weather station
+
+The basis is a small weather station based on Arduino, which is equipped with sensors.   
+The sensors are read out periodically and the data is transmitted centrally via MQTT and stored in an InfluxDB. The Data can be visualized with Grafana Dashboards.
+
+![](docs/img/atmo.png?raw=true)
+
+There is a lot to improve in this Project and far from completed, contributions are welcome.
+
+## Hardware
+
+Tested Boards:
+* Adafruit Huzzah32 Feather (ESP32)
+
+Used Sensors:
+* Adafruit BME680 (Temperature, Humidity, Pressure, Air Quality)
+* Adafruit TSL2591 (Lux)
+
+## Installation & Configurations
+
+### Configure ESP
+
+Blogpost: https://blog.sandchaschte.ch/de/posts/atmo-weather-station-part2
+
+* Install Boards & Libraries in Arduino IDE
+* `cp atmo/config.default.ino atmo/config.ino`
+* Configure WiFi and MQTT Settings in `atmo/config.ino`
+* Compile & Upload.
+
+### Install MQTT, InfluxDB, Grafana
+
+* `docker-compose up`
+
+Default User in InfluxDB, MQTT and Bridge is `atmo` and Password is `IOTRocks!`.   
+
+To change Passwords: 
+* Create File `.secrets`   
+
+```
+INFLUXDB_USER_PASSWORD=<my_password>   
+BRIDGE_INFLUXDB_PASSWORD=<my_password>   
+BRIDGE_MQTT_PASSWORD=<my_password>   
+```
+
+* Update Password in `mqtt/passwd`   
+
+```
+mosquitto_passwd -c mosquitto/passwd atmo
+```
+
+### Usage
+* MQTT Port 1883 (without TLS)
+* InfluxDB Port 8086
+* Grafana Port 3000 (http)
+
+### Grafana
+* Import Dashboard: `dashboards/atmo.json`
+
+## Links
+
+* https://blog.sandchaschte.ch/en/posts/atmo-weather-station/
--- bridge/bridge.py
+++ bridge/bridge.py
@@ -15,15 +15,17 @@ from influxdb import InfluxDBClient
 TLS_CERT_PATH = '/etc/pki/tls/cert.pem'
 
 INFLUXDB_ADDRESS = os.getenv('BRIDGE_INFLUXDB_ADDRESS', 'localhost')
-INFLUXDB_USER = os.getenv('BRIDGE_INFLUXDB_USER', 'admin')
-INFLUXDB_PASSWORD = os.getenv('BRIDGE_INFLUXDB_PASSWORD')
-INFLUXDB_DATABASE = os.getenv('BRIDGE_INFLUXDB_DATABASE')
+INFLUXDB_USER = os.getenv('BRIDGE_INFLUXDB_USER', 'atmo')
+INFLUXDB_PASSWORD = os.getenv('BRIDGE_INFLUXDB_PASSWORD', 'IOTRocks!')
+INFLUXDB_DATABASE = os.getenv('BRIDGE_INFLUXDB_DATABASE', 'atmo')
 
 MQTT_ADDRESS = os.getenv('BRIDGE_MQTT_ADDRESS', 'localhost')
-MQTT_USER = os.getenv('BRIDGE_MQTT_USER')
-MQTT_PASSWORD = os.getenv('BRIDGE_MQTT_PASSWORD')
-MQTT_TOPIC = '/sensors/+/+'  # /[raspi1|raspi2]/[motion|light|cam?]
-MQTT_REGEX = '/sensors/([^/]+)/([^/]+)/([^/]+)'
+MQTT_PORT = os.getenv('BRIDGE_MQTT_PORT', 1883)
+MQTT_USER = os.getenv('BRIDGE_MQTT_USER', 'atmo')
+MQTT_PASSWORD = os.getenv('BRIDGE_MQTT_PASSWORD', 'IOTRocks!')
+MQTT_SECURE = os.getenv('BRIDGE_MQTT_SECURE', False)
+MQTT_TOPIC = '/sensors/+/+'  # /sensors/[atmo]/temperature
+MQTT_REGEX = '/sensors/([^/]+)/([^/]+)'
 MQTT_CLIENT_ID = 'MQTTInfluxDBBridge'
 
 influxdb_client = InfluxDBClient(INFLUXDB_ADDRESS, 8086, INFLUXDB_USER, INFLUXDB_PASSWORD, None)
@@ -87,13 +89,15 @@ def main():
     _init_influxdb_database()
 
     mqtt_client = mqtt.Client(MQTT_CLIENT_ID)
-#    mqtt_client.tls_set(ca_certs=TLS_CERT_PATH, tls_version=ssl.PROTOCOL_TLSv1_2)
-#    mqtt_client.tls_insecure_set(False)
+    if MQTT_SECURE:
+        MQTT_PORT = 8883
+        mqtt_client.tls_set(ca_certs=TLS_CERT_PATH, tls_version=ssl.PROTOCOL_TLSv1_2)
+        mqtt_client.tls_insecure_set(False)
     mqtt_client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
     mqtt_client.on_connect = on_connect
     mqtt_client.on_message = on_message
 
-    mqtt_client.connect(MQTT_ADDRESS, 1883)
+    mqtt_client.connect(MQTT_ADDRESS, MQTT_PORT)
     mqtt_client.loop_forever()
 
 
